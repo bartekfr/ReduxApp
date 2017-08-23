c96a9d1698873b185af3e34c53d06323af82f8b1
Bartek Fryzowicz <bartekfr@wp.pl>
2017-08-23 10:53:30 +0200

    adds redux-devtools

--- js/store.js
+++ js/store.js
@@ -1,13 +1,18 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import reducers from './reducers';
 import createSagaMiddleware from 'redux-saga';
 import mySaga from './actions//saga'
+
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const sagaMiddleware = createSagaMiddleware();
 const store = createStore(
 	reducers,
 	{},
-	applyMiddleware(sagaMiddleware)
+	composeEnhancers(
+		applyMiddleware(sagaMiddleware)
+	)
 );
 
 sagaMiddleware.run(mySaga);
